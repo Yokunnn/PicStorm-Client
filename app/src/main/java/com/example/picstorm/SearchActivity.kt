372ebc0a4d19e74ac2187319aba56cb01ef19f3a
Yokunnn <e.zakablukov@gmail.com>
2023-04-25 00:05:57 +0300

    add nav and search layouts, add hilt and navigation component dependencies

--- app/src/main/java/com/example/picstorm/SearchActivity.kt
+++ app/src/main/java/com/example/picstorm/SearchActivity.kt
@@ -2,15 +2,15 @@ package com.example.picstorm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.picstorm.databinding.ActivityRegisterBinding
+import com.example.picstorm.databinding.ActivitySearchBinding
 
-class RegisterActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var binding: ActivitySearchBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
